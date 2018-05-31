cda7d1e73bc0f4d4126fa1006f3fcaa1302c0b6e
unknown <hieupvd00585@fbt.edu.vn>
2018-05-31 10:28:17 +0700

    lan dau tai file

---
+++ ConsoleApplication6/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleApplication6.Properties;
+
+namespace ConsoleApplication6
+{
+    internal class Program
+    {
+        private static List<Student> list = new List<Student>();
+        public static void Main(string[] args)
+        {
+            hieuMenu();
+        }
+
+        private static void SearchByName()
+        {
+            Console.WriteLine("Please enter name to search:");
+            var searchKey = Console.ReadLine();
+            for (int i = 0; i < list.Count; i++)
+            {
+                Student student = list[i];
+                if (student.Name == searchKey)
+                {
+                    Console.WriteLine("Found:");
+                    Console.WriteLine(student.Name+ "-" + student.RollNumber);
+                }
+            }
+
+    
+        }
+
+        private static void AddStudent()
+        
+        {
+            var student = new Student();
+            Console.WriteLine("Please enter student name :");
+            student.Name = Console.ReadLine();
+            Console.WriteLine("Please enter student rollnumber :");
+            student.RollNumber = Console.ReadLine();
+            Console.WriteLine("Please enter student email :");
+            student.Email = Console.ReadLine();
+            Console.WriteLine("Please enter student phone:");
+            student.Phone = Console.ReadLine();
+            list.Add(student);
+            
+
+        }
+
+        private static void DisplayStudent()
+        {
+            foreach (var student in list)
+            {
+                Console.WriteLine(student.Name);
+            }
+            
+        }
+
+        private static void hieuMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("-------------StudentManager---------------");
+                Console.WriteLine("1.Add new student");
+                Console.WriteLine("2.Show list student.");
+                Console.WriteLine("3.Search student by name");
+                Console.WriteLine("4.Exit");
+                Console.WriteLine("------------------------------------------");
+                Console.WriteLine("Please enter your choice:");
+                var choice = Int32.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                      case 1:
+                          AddStudent();
+                          break;
+                      case 2:
+                          DisplayStudent();
+                          break;
+                      case 3:
+                          SearchByName();
+                          break;
+                      case  4:
+                          Environment.Exit(1);
+                          break;
+                }
+            }
+            
+        }
+
+    }
+}
---
+++ ConsoleApplication6/Properties/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApplication6.Properties
+{
+    public class Student
+    {
+        private String name;
+        private String rollNumber;
+        private String email;
+        private String phone;
+
+        public Student(string name, string rollNumber, string email, string phone)
+        {
+            this.name = name;
+            this.rollNumber = rollNumber;
+            this.email = email;
+            this.phone = phone;
+        }
+
+        public Student()
+        {
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string RollNumber
+        {
+            get { return rollNumber; }
+            set { rollNumber = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        public override string ToString()
+        {
+            return this.email + this.rollNumber + this.email + this.phone;
+        }
+    }
+}
